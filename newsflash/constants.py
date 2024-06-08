ad91ac7e99ba085e7affa6cf6c954fb655347d7e
Dragos Carmaciu <dragman@gmail.com>
2024-06-08 12:45:44 +0100

    Add support for thriplashes

--- newsflash/constants.py
+++ newsflash/constants.py
@@ -2,3 +2,16 @@ IGNORE_ABSTRACTS = {
     "The latest five minute news bulletin from BBC World Service.",
     "Continuing coverage of the 2024 General Election Campaign, from BBC News",
 }
+
+SYSTEM_PROMPT = (
+    "You are a helpful assistant that generates 'Quiplash' prompts from news headline|abstract pairs. You may also be explicitly asked to generate "
+    "'Thriplash' prompts. A 'Thriplash' prompt is a special round in the game Quiplash where players have to provide three funny responses to a given prompt. "
+    "For example: 'Three things you might find in a wizard's pocket.' Please do not enclose prompts in quotation marks or try to answer prompts yourself, "
+    "please just generate prompts under 120 characters and try to leave room for creativity from the players.  When shortneing names from headlines prefer surnames."
+)
+
+STANDARD_PROMPT = "Turn this news headline|abstract into standard Quiplash prompt: {headline}|{abstract}"
+
+THRIPLASH_PROMPT = (
+    "Turn this news headline|abstract into a 'Thriplash' prompt: {headline}|{abstract}"
+)
--- newsflash/script.py
+++ newsflash/script.py
@@ -10,7 +10,12 @@ import requests
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from newsflash.constants import IGNORE_ABSTRACTS
+from newsflash.constants import (
+    IGNORE_ABSTRACTS,
+    STANDARD_PROMPT,
+    SYSTEM_PROMPT,
+    THRIPLASH_PROMPT,
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -23,6 +28,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
 SupportedModels = Literal["gpt-4-turbo", "gpt-3.5-turbo"]
 MAX_TOKENS = 50
+MAX_CHARS = 120
 
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 NEWS_API_SOURCES = ["bbc-news", "bbc-sport"]
@@ -112,39 +118,56 @@ def save_articles(run_date: datetime.date, lookback: int, max_pages: int = 1) ->
 
 
 def generate_quiplash_prompt(
-    headline: str, abstract: str, model: SupportedModels, is_wet: bool = False
+    headline: str,
+    abstract: str,
+    model: SupportedModels,
+    is_wet: bool = False,
+    is_thriplash: bool = False,
 ) -> str | None:
-    gpt_prompt = (
-        "Turn this news headline into a max 120 char Quiplash prompt, do not be tempted to answer, "
-        "do not add quotation marks or use the word Quiplash or prompt:\n"
-        f"{headline}: {abstract}"
-    )
+    if is_thriplash:
+        gpt_prompt = THRIPLASH_PROMPT
+    else:
+        gpt_prompt = STANDARD_PROMPT
+
+    gpt_prompt = gpt_prompt.format(headline=headline, abstract=abstract)
+
     if not is_wet:
-        logger.info("Not sending prompt, dry mode!")
-        logger.debug(gpt_prompt)
+        logger.info(f"Not sending prompt, dry mode!\n{gpt_prompt}")
         return '"Testing prompts..."'
+
+    messages = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": gpt_prompt},
+    ]
 
     response = client.chat.completions.create(
         model=model,
-        messages=[{"role": "user", "content": gpt_prompt}],
+        messages=messages,
         max_tokens=MAX_TOKENS,
     )
     return response.choices[0].message.content
 
 
 def process_articles(
-    file_name: str, model: SupportedModels, is_wet: bool = False
+    file_name: str,
+    model: SupportedModels,
+    is_wet: bool = False,
+    thriplash_pct: float = 0.1,
 ) -> None:
     # Load your dictionary with headlines and abstracts
     with open(file_name, "r") as file:
         headlines_and_abstracts = json.load(file)
 
+    # Figure out how many thriplash prompts we will ame
+    thriplash_threshold = (1 - thriplash_pct) * len(headlines_and_abstracts)
+
     # Generate prompts for each abstract
     quiplash_prompts: list[dict[str, str]] = []
-    for headline, abstract in headlines_and_abstracts.items():
+    for i, (headline, abstract) in enumerate(headlines_and_abstracts.items()):
+        is_thriplash = i > thriplash_threshold
         logger.info(f"Sending: {headline}")
         quiplash_prompt = generate_quiplash_prompt(
-            headline, abstract, model, is_wet=is_wet
+            headline, abstract, model, is_wet=is_wet, is_thriplash=is_thriplash
         )
         if quiplash_prompt is None:
             logger.warning("Nothing returned from GPT!")
@@ -155,6 +178,7 @@ def process_articles(
                 "headline": headline,
                 "abstract": abstract,
                 "quiplash_prompt": quiplash_prompt.strip("\"'"),
+                "is_thriplash": is_thriplash,
             }
         )
 
@@ -162,8 +186,19 @@ def process_articles(
     with open(f"{file_name}_quiplash_prompts.json", "w") as file:
         json.dump(quiplash_prompts, file, indent=4)
 
+    in_thriplash_section = False
     with open(f"{file_name}_quiplash_prompts.txt", "w") as file:
-        file.writelines([item["quiplash_prompt"] + "\n\n" for item in quiplash_prompts])
+        for quiplash_prompt in quiplash_prompts:
+            if not in_thriplash_section:
+                if quiplash_prompt["is_thriplash"]:
+                    file.write("=== THRIPLASHES ===\n\n")
+                    in_thriplash_section = True
+            prompt = quiplash_prompt["quiplash_prompt"]
+            if len(prompt) > MAX_CHARS:
+                logging.warning("Skipping long prompt: {prompt}")
+                continue
+
+            file.write(prompt + "\n\n")
 
 
 @click.command()
@@ -190,6 +225,7 @@ def main(
 
     articles_file = save_articles(run_date, lookback=lookback)
     process_articles(articles_file, model, is_wet=wet)
+    logging.info("Job's done!")
 
 
 if __name__ == "__main__":
